d28431dcf9dfa196d748fff51a1c81d914464c67
Adrien <66513643+AshtakaOOf@users.noreply.github.com>
2026-04-16 16:08:34 -0400

    Exif migrations

--- Sources/VernissageServer/Application+Migrations.swift
+++ Sources/VernissageServer/Application+Migrations.swift
@@ -82,7 +82,10 @@ extension Application {
         
         self.migrations.add(PushSubscription.CreatePushSubscriptions())
         self.migrations.add(PushSubscription.CreateAmmountOfErrorsField())
-        
+
+        self.migrations.add(Exif.AddParameters())
+        self.migrations.add(Exif.AddWorkflow())
+
         self.migrations.add(UserAlias.CreateUserAliases())
         self.migrations.add(Exif.AddFilmColumn())
         self.migrations.add(User.AddUrl())
