f8e9c662705a136066b1ac111ab45d77f0173e33
Marcin Czachurski <mczachurski@icloud.com>
2026-07-07 16:00:34 +0200

    #567 Change size of description column for attachments

--- Sources/VernissageServer/Application+Migrations.swift
+++ Sources/VernissageServer/Application+Migrations.swift
@@ -210,5 +210,7 @@ extension Application {
         self.migrations.add(Status.CreateActivityPubUrlIndex())
         self.migrations.add(User.CreateMovedToUserIdIndex())
         self.migrations.add(Article.AddLanguage())
+        self.migrations.add(Attachment.ChangeDescriptionLength())
+        self.migrations.add(AttachmentHistory.ChangeDescriptionLength())
     }
 }
